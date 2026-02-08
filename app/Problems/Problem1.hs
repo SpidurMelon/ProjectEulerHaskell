4573531ad40c2574e32e249e9271fdcfdf06feb6
Julian van der Weijden <julianvdweijden@gmail.com>
2026-02-08 16:29:14 +0100

    Problem 1

--- app/Problems/Problem1.hs
+++ app/Problems/Problem1.hs
@@ -1,4 +1,10 @@
 module Problems.Problem1 where
 
+isMultiple :: Integer -> Bool
+isMultiple n = mod n 3 == 0 || mod n 5 == 0
+
 solve :: Integer
-solve = 9
+solve = sum (filter isMultiple [1 .. 999])
+
+-- >>> solve
+-- 233168
